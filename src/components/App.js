53f6b0194f65b5d9d845db7986dcbc3d6f05ee2d
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-11-01 17:59:14 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+
+import { useEffect, useState } from "react";
+
+function App(){
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then((imageData) => setData(imageData.message))
+   }, [])
+
+  if(!data) {
+    return <p>Loading...</p>
+  }
+    return (
+
+    <img src={data} alt="A Random Dog"/> 
+       
+    )
+}
+
+export default App;
